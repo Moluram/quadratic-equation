6aaf4eb41fd18397d4eceee2368e380ecd9433a3
moluram <moluram.a@gmail.com>
2018-09-20 16:31:25 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let parameters = getParameters(equation)
+
+  let a = parameters[0]
+  let b = parameters[1]
+  let c = parameters[2]
+
+  let D = b*b - 4 * a * c
+
+  let x1 = (-1 * b + Math.sqrt(D)) / (2 * a)
+  let x2 = (-1 * b - Math.sqrt(D)) / (2 * a)
+
+  if (a > 0) {
+    let temp = x1
+    x1 = x2
+    x2 = temp
+  }
+
+  return [Math.round(x1), Math.round(x2)]
+}
+
+function getParameters(equation) {
+  equation = equation.replace(/\s/g,'')
+  const regex = /([\-/+]+[0-9])+([0-9]*\.?[0-9]+)|(^[0-9]+)|([\-]+[0-9])/gm;
+  
+  return equation.match(regex)
 }
